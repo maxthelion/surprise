8974f571968afdfd83b4d74759cc84376ec12df3
Daniel Waterworth <Da.Waterworth@gmail.com>
2012-01-17 13:31:02 +0000

    whitespace changes
    Conflicts:
    
    	test.html

--- util.js
+++ util.js
@@ -4,42 +4,41 @@ var Vector = function(x, y){
 }
 
 Vector.prototype = {
-	
   add:  function(vector){
     this.x += vector.x
     this.y += vector.y
   },
-	
+
   subtract: function(vector){
     this.x = vector.x
     this.y += vector.y
   },
-	
+
   theta: function(){
      return Math.atan2(this.y, this.x);
   },
-    
+
   clone: function(){
      return new Vector(this.x, this.y);
   },
 
   rotate: function(a){
-  	var ca = Math.cos(a);
-  	var sa = Math.sin(a);
-  	with (this)
-  	{
-  		var rx = x*ca - y*sa;
-  		var ry = x*sa + y*ca;
-  		x = rx;
-  		y = ry;
-  	}
-  	return this;
+    var ca = Math.cos(a);
+    var sa = Math.sin(a);
+    with (this)
+    {
+      var rx = x*ca - y*sa;
+      var ry = x*sa + y*ca;
+      x = rx;
+      y = ry;
+    }
+    return this;
   }
 }
 
 var Point = function(vector, rotation) {
-	this.x = vector.x
-	this.y = vector.y
-	this.rotation = rotation
-	this.vector = vector
+  this.x = vector.x
+  this.y = vector.y
+  this.rotation = rotation
+  this.vector = vector
 }
